3e8253ea18e4aac650ac8445a1dff63e7f2fe00b
Guillem <guillemb4b@gmail.com>
2024-10-07 23:48:07 +0200

    Front register role

--- frontend/src/app/pages/register/register.component.ts
+++ frontend/src/app/pages/register/register.component.ts
@@ -9,9 +9,12 @@ import { ActivatedRoute, RouterModule } from '@angular/router';
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent implements OnInit{
+  registerRole: string = ''
+  
   constructor(private _route: ActivatedRoute) {}
+
   ngOnInit(): void {
-    console.log(this._route.snapshot.queryParamMap.get('role'))
+    this.registerRole = this._route.snapshot.queryParamMap.get('role') 
   }
   //crear reactive form
 }
